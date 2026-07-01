dc3f191520e8a0c2e8767863c9e419d3afefecc7
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-01 20:00:19 +0100

    2015/day_01-03

---
+++ 2015/day_02/src/main.rs
@@ -0,0 +1,47 @@
+use benchmarker::benchmark;
+use std::{env, fmt::Display, fs};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("inputs/input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end();
+
+    println!("{}", benchmark(|| run(&input)));
+}
+
+struct Output {
+    paper: usize,
+    ribbon: usize,
+}
+
+impl Display for Output {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Part 1: {}, Part 2: {}", self.paper, self.ribbon)
+    }
+}
+
+fn run(input: &str) -> Output {
+    let outputs = input.lines().map(|x| {
+        let mut dims: Vec<usize> = x.split("x").map(|y| y.parse().unwrap()).collect();
+        dims.sort();
+
+        Output {
+            paper: 3 * (dims[0] * dims[1]) + 2 * (dims[0] * dims[2] + dims[1] * dims[2]),
+            ribbon: 2 * (dims[0] + dims[1]) + dims[0] * dims[1] * dims[2],
+        }
+    });
+
+    let mut output = Output {
+        paper: 0,
+        ribbon: 0,
+    };
+
+    for value in outputs {
+        output.paper += value.paper;
+        output.ribbon += value.ribbon;
+    }
+
+    output
+}
