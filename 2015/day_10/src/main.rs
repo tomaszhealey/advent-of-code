b6270f4e3e0bfb07b35d91c3efe797a9acd380e4
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-05 17:00:19 +0100

    2015/day_10 & 11

---
+++ 2015/day_10/src/main.rs
@@ -0,0 +1,45 @@
+use benchmarker::benchmark;
+use std::{env, fs};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end();
+
+    println!("Part 1: {}", benchmark(|| solve(input, 40)));
+    //It would of course be better to print the 40th while finding the 50th
+    println!("Part 2: {}", benchmark(|| solve(input, 50)));
+}
+
+fn solve(input: &str, repeat: usize) -> usize {
+    let mut num = String::from(input);
+
+    for _ in 0..repeat {
+        let mut chars = num.chars();
+        let mut count = 1usize;
+        let mut digit: char = chars.next().unwrap();
+        let mut new = String::new();
+
+        for c in chars {
+            if c != digit {
+                push_new(count, &digit, &mut new);
+                count = 1;
+                digit = c;
+            } else {
+                count += 1;
+            }
+        }
+
+        push_new(count, &digit, &mut new);
+        num = new;
+    }
+
+    num.to_string().len()
+}
+
+fn push_new(count: usize, digit: &char, new: &mut String) {
+    new.push_str(&count.to_string());
+    new.push(*digit);
+}
---
+++ 2015/day_11/src/main.rs
@@ -0,0 +1,109 @@
+use benchmarker::{benchmark, benchmark_return};
+use std::{collections::HashMap, env, fs, ops::RangeInclusive};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end();
+    let mut solver = Solver::new();
+
+    let (result, elapsed) = benchmark_return(|| solver.get_next(&input));
+    println!("Part 1: {result}, {elapsed}");
+    println!("Part 2: {}", benchmark(|| solver.get_next(&result)));
+}
+
+struct Solver {
+    chars: [char; 23],
+    index_map: HashMap<char, usize>,
+    straights: Vec<RangeInclusive<usize>>,
+    pairs: Vec<RangeInclusive<usize>>,
+}
+
+impl Solver {
+    fn new() -> Solver {
+        let chars = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        let mut index_map = HashMap::new();
+        for (index, c) in chars.iter().enumerate() {
+            index_map.insert(*c, index);
+        }
+        index_map.insert('i', index_map[&'j'] - 1);
+        index_map.insert('o', index_map[&'p'] - 1);
+        index_map.insert('l', index_map[&'m'] - 1);
+
+        Solver {
+            chars,
+            index_map,
+            straights: Vec::new(),
+            pairs: Vec::new(),
+        }
+    }
+
+    fn get_next(&mut self, input: &str) -> String {
+        let mut password: [char; 8] = input.chars().collect::<Vec<_>>().try_into().unwrap();
+
+        loop {
+            self.increment_char(&mut password, 7);
+            if !(password.contains(&'i') || password.contains(&'o') || password.contains(&'l')) {
+                break;
+            }
+        }
+
+        loop {
+            self.straights.clear();
+            self.pairs.clear();
+
+            for index in 0..7 {
+                self.add_straight(&password.map(|x| x as isize), index);
+                self.add_pair(&password, index);
+            }
+
+            if self.straights.len() > 0
+                && (self.pairs.len() > 3
+                    || (self.pairs.len() > 1
+                        && self.pairs[0].end() != self.pairs[1].start()
+                        && self.pairs[0].start() != self.pairs[1].end()))
+            {
+                break;
+            }
+
+            self.increment_char(&mut password, 7);
+        }
+
+        password
+            .iter()
+            .fold(String::new(), |acc, x| acc + &x.to_string())
+    }
+
+    fn increment_char(&mut self, str: &mut [char; 8], index: usize) {
+        let i = self.index_map[&str[index]];
+
+        if i == 22 {
+            self.increment_char(str, index - 1);
+            str[index] = self.chars[0];
+        } else {
+            str[index] = self.chars[i + 1];
+        }
+    }
+
+    fn add_straight(&mut self, str: &[isize; 8], index: usize) {
+        if index < 6 {
+            let a = str[index];
+            if str[index + 2] - a == 2 && str[index + 1] - a == 1 {
+                self.straights.push((index)..=index + 2);
+            }
+        }
+    }
+
+    fn add_pair(&mut self, str: &[char; 8], index: usize) {
+        if index < 7 {
+            if str[index] == str[index + 1] {
+                self.pairs.push(index..=(index + 1))
+            }
+        }
+    }
+}
