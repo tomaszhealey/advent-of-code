b5a0da43c6c0f53f93d4239a12a629b9042c8b8f
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-06 23:57:49 +0100

    2015/day_12

---
+++ 2015/day_12/src/main.rs
@@ -0,0 +1,49 @@
+use benchmarker::benchmark;
+use regex::Regex;
+use serde_json::Value;
+use std::{env, fs};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end();
+
+    println!("Part 1: {}", benchmark(|| part1(&input)));
+    println!("Part 2: {}", benchmark(|| part2(&input)));
+}
+
+fn part1(input: &str) -> isize {
+    let re = Regex::new(r"\d+|-\d+").unwrap();
+    re.find_iter(input)
+        .map(|x| x.as_str().parse::<isize>().unwrap())
+        .sum::<isize>()
+}
+
+fn part2(input: &str) -> i64 {
+    let v: Value = serde_json::from_str(input).unwrap();
+    sum(&v)
+}
+
+fn sum(val: &Value) -> i64 {
+    if let Value::Object(obj) = val {
+        if obj.values().any(|val| val == "red") {
+            return 0;
+        } else {
+            return obj.values().fold(0, |acc, x| acc + process_val(x));
+        }
+    } else if let Value::Array(arr) = val {
+        arr.iter().fold(0i64, |acc, x| acc + process_val(x))
+    } else {
+        0
+    }
+}
+
+fn process_val(v: &Value) -> i64 {
+    match v {
+        Value::Number(num) => num.as_i64().unwrap(),
+        Value::Array(_) | Value::Object(_) => sum(v),
+        _ => 0,
+    }
+}
---
+++ 2015/day_13/src/main.rs
@@ -0,0 +1,81 @@
+use benchmarker::benchmark;
+use std::{collections::HashMap, env, fs, hash::Hash};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let solver = Solver::new(input.trim_end());
+
+    println!("Part 1: {}", benchmark(|| solver.part1()));
+}
+
+struct Solver<'a> {
+    graph: HashMap<&'a str, HashMap<&'a str, isize>>,
+}
+
+impl Solver<'_> {
+    fn new(input: &str) -> Solver<'_> {
+        let mut output = HashMap::new();
+        let mut mult: isize;
+
+        for line in input.lines().map(|x| x.split(" ").collect::<Vec<_>>()) {
+            mult = if line[2] == "gain" { 1 } else { -1 };
+            let person = output.entry(line[0]).or_insert(HashMap::new());
+            person.insert(
+                line[10].strip_suffix(".").unwrap(),
+                mult * line[3].parse::<isize>().unwrap(),
+            );
+        }
+
+        Solver { graph: output }
+    }
+
+    fn part1(&self) -> isize {
+        self.find_best(
+            &mut vec![""; self.graph.keys().len()],
+            self.graph.keys().map(|x| *x).collect(),
+        )
+    }
+
+    fn part2(&mut self) -> isize {
+        self.graph.insert("me", HashMap::new());
+        for key in self.graph.keys() {
+            self.graph.get_mut(key).insert("me", 0);
+            self.graph["me"].insert(key, 0);
+        }
+
+        self.part1()
+    }
+
+    fn find_best<'a>(&self, table: &mut Vec<&'a str>, stack: Vec<&'a str>) -> isize {
+        if stack.len() == 0 {
+            let len = table.len();
+            let mut acc = self.graph[table[0]][table[len - 1]] + self.graph[table[0]][table[1]];
+            for i in 1..table.len() {
+                acc +=
+                    self.graph[table[i]][table[i - 1]] + self.graph[table[i]][table[(i + 1) % len]];
+            }
+            return acc;
+        }
+
+        let mut best = isize::MIN;
+        for next in 0..stack.len() {
+            let new: Vec<&str> = stack
+                .clone()
+                .iter()
+                .filter(|x| **x != stack[next])
+                .map(|x| *x)
+                .collect();
+            table[self.graph.len() - stack.len()] = stack[next];
+
+            let out = self.find_best(table, new);
+            if out > best {
+                best = out;
+            }
+        }
+
+        best
+    }
+}
