a04eb12bc4f8bc77bd7f33650c287fe6b115d1be
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-10 13:00:49 +0100

    2015/day_20

---
+++ 2015/day_20/src/main.rs
@@ -0,0 +1,86 @@
+use benchmarker::benchmark;
+use itertools::Itertools;
+use std::{collections::HashSet, env, fs};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end().parse::<usize>().unwrap();
+    println!("Part 1: {}", benchmark(|| part1(input)));
+    println!("Part 2: {}", benchmark(|| part2(input)));
+}
+
+fn factors(mut num: usize) -> Vec<usize> {
+    let mut factors = vec![];
+    while num % 2 == 0 {
+        factors.push(2);
+        num /= 2;
+    }
+
+    let mut d = 3;
+    while d * d <= num {
+        while num % d == 0 {
+            factors.push(d);
+            num /= d;
+        }
+        d += 2;
+    }
+
+    if num > 1 {
+        factors.push(num);
+    }
+
+    factors
+}
+
+fn part1(target: usize) -> usize {
+    let mut i = 2;
+    let mut presents = 0;
+    let mut computed = HashSet::new();
+    while presents < target {
+        i += 2; // answer will be even
+        let factors = factors(i);
+        presents = 0;
+        let pset = factors.into_iter().powerset();
+        computed.clear();
+
+        for set in pset {
+            if computed.contains(&set) {
+                continue;
+            }
+            presents += 10 * set.iter().product::<usize>();
+            computed.insert(set);
+        }
+    }
+
+    i
+}
+
+fn part2(target: usize) -> usize {
+    let mut i = 1;
+    let mut presents = 0;
+    let mut computed = HashSet::new();
+    while presents < target {
+        i += 1;
+        let factors = factors(i);
+        presents = 0;
+        let pset = factors.into_iter().powerset();
+        computed.clear();
+
+        for set in pset {
+            if computed.contains(&set) {
+                continue;
+            }
+            let p = set.iter().product::<usize>();
+            computed.insert(set);
+            if p * 50 < i {
+                continue;
+            }
+            presents += 11 * p;
+        }
+    }
+
+    i
+}
