ce452f0a28bf99f219986facf64a5567e9d28776
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-12 23:00:41 +0100

    2015/day_25

---
+++ 2015/day_25/src/main.rs
@@ -0,0 +1,33 @@
+use benchmarker::benchmark;
+use std::{env, fs};
+
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .unwrap_or_else(|| String::from("input.txt"));
+    let input = fs::read_to_string(file_path).expect("Error reading file.");
+    let input = input.trim_end();
+    let nums = input.split("row ").collect::<Vec<_>>()[1]
+        .split(", column ")
+        .collect::<Vec<_>>();
+    let (row, col): (usize, usize) = (
+        nums[0].parse().unwrap(),
+        nums[1].strip_suffix(".").unwrap().parse().unwrap(),
+    );
+
+    println!("Part 1: {}", benchmark(|| get_code(row, col)));
+}
+
+fn triangle_num(n: usize) -> usize {
+    (n * (n + 1)) / 2
+}
+
+fn get_code(row: usize, col: usize) -> usize {
+    let code_position = triangle_num(col + (row - 1)) - row;
+    let mut code = 20151125;
+    for _ in 0..code_position {
+        code = (code * 252533) % 33554393;
+    }
+
+    code
+}
