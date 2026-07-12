1bd828210bb4629d9492abbe15aabcb5bae307a7
THealey235 <122405008+THealey235@users.noreply.github.com>
2026-07-12 23:33:11 +0100

    Updated 2015/day_03

--- 2015/day_03/src/main.rs
+++ 2015/day_03/src/main.rs
@@ -4,7 +4,7 @@ use std::{collections::HashSet, env, fs};
 fn main() {
     let file_path = env::args()
         .nth(1)
-        .unwrap_or_else(|| String::from("inputs/input.txt"));
+        .unwrap_or_else(|| String::from("input.txt"));
     let input = fs::read_to_string(file_path).expect("Error reading file.");
 
     println!("Part 1: {}", benchmark(|| part1(&input)));
